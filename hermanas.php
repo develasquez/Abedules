32210cce29a55f6632f567edef1679b89890d530
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-07-12 03:14:48 -0400

    Cambio Conexion a bd

--- hermanas.php
+++ hermanas.php
@@ -3,16 +3,15 @@
 
 
 
-
 function Conectarse() 
 { 
    
-   if (!($link=mysql_connect("localhost","k25216_escuela","escuela2011"))) 
+   if (!($link=mysql_connect("localhost","root","devenew1"))) 
    { 
       echo '{"success":true, "data":[], "errors":"Error al conectar con la Base de Datos" }'; 
       exit(); 
    } 
-   if (!mysql_select_db("k25216_escuela",$link)) 
+   if (!mysql_select_db("abedules",$link)) 
    { 
       echo '{"success":true, "data":[], "errors":"Error seleccionando la base de datos" }'; 
       exit(); 
--- programa.php
+++ programa.php
@@ -1,5 +1,22 @@
 <? /*asignaciones de hermanos ordenadas por fecha 
 
+function Conectarse() 
+{ 
+   
+   if (!($link=mysql_connect("localhost","root","devenew1"))) 
+   { 
+      echo '{"success":true, "data":[], "errors":"Error al conectar con la Base de Datos" }'; 
+      exit(); 
+   } 
+   if (!mysql_select_db("abedules",$link)) 
+   { 
+      echo '{"success":true, "data":[], "errors":"Error seleccionando la base de datos" }'; 
+      exit(); 
+   } 
+   return $link; 
+} 
+
+
 select
 h.id,
 h.nombre,
