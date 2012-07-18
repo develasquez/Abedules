a96639be76cb0b078560065c1200313fe5efee22
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-07-18 01:33:26 -0400

    as

--- graba.php
+++ graba.php
@@ -20,22 +20,6 @@ function Conectarse()
 
 
 
- $update="update hermanos set".
-    ' nombre ="'. $_POST["nombre"].'"'.   
-    ' ,apellido_paterno="'.$_POST["apellido_paterno"].'"'.
-    ' ,apellido_materno="'.$_POST["apellido_materno"].'"'.
-    ' ,apellido_casada"'.$_POST["apellido_casada"].'"'.
-    ' ,direccion"'.$_POST["direccion"].'"'.
-    ' ,telefono="'.$_POST["telefono"].'"'.
-    ' ,tipo_sexo='.$_POST["tipo_sexo"].
-    ' ,tipo_privilegio='.$_POST["tipo_privilegio"].
-    ' ,nivel_escuela='.$_POST["nivel_escuela"].
-    ' ,activo='.$_POST["activo"].
-    ' ,vigente='.$_POST["vigente"].
-    ' ,participa='.$_POST["participa"].
-    ' ,id_grupo='.$_POST["grupo"].
-    ' where id= '.$_REQUEST["id"].";";
-    
-    echo ($update);
+
 
 ?>
