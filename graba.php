a01c58345aa7b15c9c5bb1c38ac2573738be012c
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-07-18 00:34:39 -0400

    456

--- graba.php
+++ graba.php
@@ -17,19 +17,7 @@ function Conectarse()
 } 
 
 
-$nombre = $_POST["nombre"];
-$apellido_paterno    = $_POST["apellido_paterno"];
-$apellido_materno = $_POST["apellido_materno"];
-$apellido_casada  = $_POST["apellido_casada"];
-$direccion  = $_POST["direccion"];
-$telefono   = $_POST["telefono"];
-$tipo_sexo  = $_POST["tipo_sexo"];
-$tipo_privilegio  = $_POST["tipo_privilegio"];
-$nivel_escuela = $_POST["nivel_escuela"];
-$activo  = $_POST["activo"];
-$vigente = $_POST["vigente"];
-$participa  = $_POST["participa"];
-$id_grupo  = $_POST["id_grupo"];
+
 
 
  $update="update hermanos set".
